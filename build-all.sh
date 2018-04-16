37c09c8fe17ca1c574fa4821c81d52cf78c4d656
Zbigniew Romanowski <romz@wp.pl>
2018-04-16 18:10:59 +0200

    Add cmake compilation framework

---
+++ build-all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#
+# Create 'build' directory
+#
+rm -rf build || exit 1
+mkdir build || exit 1
+cd build || exit 1
+
+#
+# Run cmake
+#
+
+cmake -DBUILD_GTEST=ON -DBUILD_GMOCK=OFF -DCMAKE_BUILD_TYPE=Debug -Dbuild_tests=ON .. || exit 1
+make -j4 || exit 1
+
+
+#
+# Run test
+#
+ctest --verbose
+
+
